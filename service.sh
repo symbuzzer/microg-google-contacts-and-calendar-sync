d5c47b952ccdfaec0b1631835f200f5e0db416fa
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-07-05 17:28:44 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -2,3 +2,6 @@ until [ $(getprop sys.boot_completed). = 1. ]; do sleep 1; done
 pm grant "com.google.android.syncadapters.contacts" android.permission.READ_CONTACTS
 pm grant "com.google.android.syncadapters.contacts" android.permission.WRITE_CONTACTS
 pm grant "com.google.android.syncadapters.contacts" com.google.android.googleapps.permission.GOOGLE_AUTH.cp
+pm grant "com.google.android.syncadapters.calendar" android.permission.READ_CALENDAR
+pm grant "com.google.android.syncadapters.calendar" android.permission.WRITE_CALENDAR
+pm grant "com.google.android.syncadapters.calendar" com.google.android.googleapps.permission.GOOGLE_AUTH.cp
